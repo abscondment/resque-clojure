e7113abaf04056f0b58a56915eebd047c8f5ef13
Paul Barry <mail@paulbarry.com>
2012-08-14 14:22:12 -0400

    Point at my fork to deploy to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject resque-clojure "0.2.2"
   :description "Redis based library for asynchronous processing"
-  :url "https://github.com/jxa/resque-clojure"
+  :url "https://github.com/pjb3/resque-clojure"
   :dependencies [[org.clojure/data.json "0.1.2"]
                  [redis.clients/jedis "1.5.2"]]
   :dev-dependencies [[org.clojure/clojure "1.3.0"]])
